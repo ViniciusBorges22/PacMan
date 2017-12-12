b6937ba227844f5fc9e0b08258494025797e3a7b
ViniciusBorges <viniciusborges22@gmail.com>
2017-12-12 12:09:26 -0200

    Movimentação do pacman mais fluida

--- src/utils/Consts.java
+++ src/utils/Consts.java
@@ -10,7 +10,7 @@ public class Consts {
 
     public static final int WALK_STEP_DEC_PLACES = 1;
 
-    public static final int DELAY = 10;
+    public static final int DELAY = 5;
 
     public static final int TIMER_CHERRY = 10000;
 
@@ -22,7 +22,7 @@ public class Consts {
 
     public static final double WALK_STEP_ENEMY = 0.03;
 
-    public static final double WALK_STEP = 0.2;
+    public static final double WALK_STEP = 0.02;
 
     public static final String PATH = File.separator + "imgs" + File.separator;
 }
