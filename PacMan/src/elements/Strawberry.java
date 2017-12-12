e977905249cf070c8f85ac91b0ca292eb4f4308c
lllgabrielll <evangelista.gr@live.com>
2017-12-12 00:25:06 -0200

    Add files via upload

---
+++ PacMan/src/elements/Strawberry.java
@@ -0,0 +1,10 @@
+package elements;
+
+public class Strawberry extends Fruit {
+
+    public Strawberry() {
+        super("strawberry.png");
+        this.points = 300;
+        this.duration = 10000;
+    }
+}
