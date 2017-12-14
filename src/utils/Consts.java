7622987bcf803c31913b9f719643817ab331bfcc
ViniciusBorges <viniciusborges22@gmail.com>
2017-12-14 15:56:05 -0200

    velocidade e fluidez

--- src/utils/Consts.java
+++ src/utils/Consts.java
@@ -10,7 +10,7 @@ public class Consts {
 
     public static final int WALK_STEP_DEC_PLACES = 1;
 
-    public static final int DELAY = 10;
+    public static final int DELAY = 5;
 
     public static final int TIMER_CHERRY = 50000;
 
@@ -18,13 +18,13 @@ public class Consts {
 
     public static final int BALL_SIZE = 3;
 
-    public static final double WALK_STEP_FRUIT = 0.02;
+    public static final double WALK_STEP_FRUIT = 0.01;
 
-    public static final double WALK_STEP_ENEMY = 0.05;
+    public static final double WALK_STEP_ENEMY = 0.02;
     
-    public static final double WALK_STEP_SLOW_ENEMY = 0.02;
+    public static final double WALK_STEP_SLOW_ENEMY = 0.01;
 
-    public static final double WALK_STEP = 0.1;
+    public static final double WALK_STEP = 0.05;
     
     public static final String PATH = File.separator + "imgs" + File.separator;
 }
